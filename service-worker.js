be2cb1504ef1ae6fb93761c2e8ebf121fbad8a82
SCUSTraining <weathersunclay@gmail.com>
2025-09-14 20:36:18 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,26 @@
-const CACHE_NAME = 'fr-gamehub-v1';
+---
+---
+const CACHE_NAME = 'fr-gamehub-v4';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './games/Fault_Response_SOLO_Trainer_v2.html',
-  './games/Fault_Response_SOLO_Trainer_30Q_Badges.html',
-  './games/Fault_Response_Jeopardy_Interactive.html'
+  '{{ "/" | relative_url }}',
+  '{{ "/index.html" | relative_url }}',
+  '{{ "/manifest.webmanifest" | relative_url }}',
+  '{{ "/icons/icon-192.png" | relative_url }}',
+  '{{ "/icons/icon-512.png" | relative_url }}',
+  '{{ "/games/Fault_Response_SOLO_Trainer_v2.html" | relative_url }}',
+  '{{ "/games/Fault_Response_SOLO_Trainer_30Q_Badges.html" | relative_url }}',
+  '{{ "/games/Fault_Response_Jeopardy_Interactive.html" | relative_url }}'
 ];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))));
-});
-self.addEventListener('fetch', (e) => {
-  const req = e.request;
+self.addEventListener('install', e =>
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)))
+);
+self.addEventListener('activate', e =>
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME?caches.delete(k):null))))
+);
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
-      return res;
-    }).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      const copy = res.clone(); caches.open(CACHE_NAME).then(c => c.put(e.request, copy)); return res;
+    }).catch(() => caches.match('{{ "/index.html" | relative_url }}')))
   );
 });
